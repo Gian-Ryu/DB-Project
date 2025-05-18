6341161c52299ffdd6e094452ef7e1666d4887c4
Gian Ryu <144473604+Gian-Ryu@users.noreply.github.com>
2025-05-17 20:15:37 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import mysql.connector
+
+def get_database_connection():
+  connection = mysql.connector.connect(user='gianr4',
+                                      password='233157387',
+                                      host='10.8.37.226',
+                                      database='gianr4_db')
+  return connection
+
+def execute_statement(connection, statement):
+  cursor = connection.cursor()
+  cursor.execute(statement)
+  results = []
+
+  for row in curson:
+    results.append(row)
+
+  cursor.close()
+  connection.close()
+
+  return results
+
+def get_student_schedule(student_id):
+  statement = "CALL get_class_info(" + student_id + ")"
+  return execute_statement(get_database_connection(), statement)
+
+student_id = input("Choose a student id: ")
+results = get_student_schedule(student_id)
+
+for row in cursor:
+   print()
+   period = result[1]
+   course = result[2]
+   room = result[3]
+   teacher = result[4]
+   print("Period: " + period)
+   print("Course: " + course)
+   print("Room: " + room)
+   print("Teacher: " + teacher)
+
+
+
